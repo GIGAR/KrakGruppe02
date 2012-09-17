be9c5eee25871bd43396830962cfef1f71703428
Mikkel Stolborg <mstolbor@gmail.com>
2012-09-17 16:57:53 +0200

    Test of class

---
+++ src/java/itu/dk/smds/e2012/common/AlexMan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itu.dk.smds.e2012.common;
+
+/**
+ *
+ * @author mhs
+ */
+public class AlexMan {
+    
+}
